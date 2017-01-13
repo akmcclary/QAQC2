eedd8d0d5f3c2a8c9584b780e52179b51bdddb77
akmcclary <akmcclary@gmail.com>
2017-01-13 14:13:35 -0800

    Adding more dropdowns

--- QAQC/app.R
+++ QAQC/app.R
@@ -15,11 +15,13 @@ library(plotly)
 library(shiny)
 library("magrittr")
 library("shinydashboard")
+library("DT")
+library("readr")
 #Open connection to SQL Server using system DSN
 cn <- odbcConnect("RDSN",uid="Coho_Admin",pwd = "R0ppP1&ddzos")
 #Get tagged fish from the current year using pre-written SQL View
 
-EFishingFish <- sqlQuery(cn,"SELECT Survey, Year, Species, PITNumber, ForkLength, Weight, CarcassTagObsApp, Comments, ReachName, Agency FROM dbo.tblDataIndividuals WHERE (Species = N'coho salmon') AND (Year > 2014) OR (Species = N'steelhead') AND (Year > 2014)")
+EFishingFish <- sqlQuery(cn,"SELECT IndividualID, Survey, Year, Species, PITNumber, ForkLength, Weight, Comments, ReachName, Agency FROM dbo.tblDataIndividuals WHERE (Species = N'coho salmon') AND (Year > 2014) OR (Species = N'steelhead') AND (Year > 2014)")
 EFishingFish[EFishingFish == -9999] = NA
 EFishingFish<-mutate_each(EFishingFish,funs(toupper))
 speciesList<-c("COHO SALMON", "STEELHEAD")
@@ -28,14 +30,11 @@ yearList<-c(2014,2015,2016)
 EFishingFish<- transform(EFishingFish, ForkLength = as.numeric(ForkLength))
 EFishingFish<- transform(EFishingFish, Weight = as.numeric(Weight))
 # Define UI for application that draws a histogram
-ui <- shinyUI((
-  
-  # Application title
-  titlePanel("E-Fishing Data")
+ui <- dashboardPage(
+  dashboardHeader(title = "QAQC Dashboard"),
   
   # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
+  dashboardSidebar(
       selectInput("SpeciesSelected", "Species:", speciesList, selected = "COHO SALMON"),
       selectInput("YearSelected", "Year:", yearList, selected = 2016),
       selectInput("SurveySelected", "Survey", surveyList, selected = "EF" ),
@@ -43,19 +42,41 @@ ui <- shinyUI((
     ),
     
     # Show a plot of the generated distribution
-    mainPanel(
-      plotlyOutput("lengthWeightGraph")
+    dashboardBody(
+      plotlyOutput("lengthWeightGraph"),
+      verbatimTextOutput("boxselect"),
+      verbatimTextOutput("missingmeasurements")
     )
   )
-))
+
 
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
   
   output$lengthWeightGraph<-renderPlotly({
-    fishGraph<- EFishingFish%>%filter(Species == input$SpeciesSelected)%>%filter(Year==input$YearSelected)%>%filter(Agency==input$AgencySelected)%>%filter(Survey==input$SurveySelected)%>% ggplot(aes(x = Weight, y = ForkLength, label= PITNumber)) + geom_point()
+    fishGraph<- EFishingFish%>%filter(Species == input$SpeciesSelected)%>%filter(Year==input$YearSelected)%>%filter(Agency==input$AgencySelected)%>%filter(Survey==input$SurveySelected)%>% ggplot(aes(x = Weight, y = ForkLength, label= PITNumber, key = IndividualID)) + geom_point()
     ggplotly(fishGraph)
   })
+  
+  output$boxselect<-renderPrint({
+    eventdata<-event_data("plotly_selected")
+    if(is.null(eventdata)==T)return(NULL)
+    mergedData<- merge(eventdata, EFishingFish, by.x = "key",by.y = "IndividualID" )
+    mergedData
+  }
+  
+    )
+  output$missingmeasurements<- renderPrint({
+      missingfish<-EFishingFish%>%filter(Species == input$SpeciesSelected)%>%filter(Year==input$YearSelected)%>%filter(Agency==input$AgencySelected)%>%filter(Survey==input$SurveySelected)%>%filter(is.na(Weight)|is.na(ForkLength))
+      missingfish
+  })  
+  
+  # output$DataTableSelectedPoints<-renderDataTable({
+  #   event.data<-event_data("plotly_selected")
+  #   if(is.null(event.data)==T)return(NULL)
+  #   DT::datatable(event.data, colnames = c("Survey", "Year", "Species", "PITNumber", "ForkLength", "Weight", "Comments", "ReachName", "Agency"), rownames = FALSE, options = list(order = list(list(0, 'desc')), autoWidth = FALSE))
+  #   
+  #   })
   
 })
 
